f70f6ff0570664e47a74cbf57a2a980385828cc0
Obskurcyte <hadrien.jaubert@centrale-marseille>
2021-08-31 23:38:08 +0200

    first commit

---
+++ screens/auth/LoginScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, TextInput} from 'react-native';
+import {Formik} from "formik";
+
+const Login = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Login;
